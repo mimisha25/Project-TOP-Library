7d34cfa0cd049ed2f082f532b19a52d23d8d28b1
mimisha25 <miacorporation25@gmail.com>
2024-03-31 23:02:20 -0400

    Add functions to button delete in each cards

--- index.js
+++ index.js
@@ -17,54 +17,65 @@ function Book(title, author, pages, isRead) {
   this.isRead = isRead;
 }
 
-btn.addEventListener("click", () => {
+function render(){
+    book_container.innerHTML="";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book =  myLibrary[i];
+        const new_Book = document.createElement("div");
+        new_Book.classList.add("book");
+        const new_title = document.createElement("div");
+        new_title.classList.add("book-title");
+        const new_author = document.createElement("div");
+        new_author.classList.add("book-author");
+        const new_pages = document.createElement("div");
+        new_pages.classList.add("book-pages");
+        const new_isRead = document.createElement("div");
+        new_isRead.classList.add("book-isRead");
+        const button = document.createElement("button");
+        button.classList.add("delete");
+        button.setAttribute("onclick", `removeBook(${i})`)
+       
+        new_title.textContent = `Title : ${book.title}`;
+        new_author.textContent = `Author :${book.author}`;
+        new_pages.textContent = `Number of pages :${book.pages}`;
+        new_isRead.textContent = `${book.isRead}`;
+          button.textContent='Delete'
+          book_container.appendChild(new_Book);
+        new_Book.appendChild(new_title);
+        new_Book.appendChild(new_author);
+        new_Book.appendChild(new_pages);
+        new_Book.appendChild(new_isRead);
+        new_Book.appendChild(button);     
 
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const isRead = document.querySelector("#isRead").checked
-    ? "has been read"
-    : "not read yet";
-
-
-  const book = new Book(title, author, pages, isRead);
-  myLibrary.push(book);
+    }
   
-  const new_Book = document.createElement("div");
-  new_Book.classList.add("book");
-  const new_title = document.createElement("div");
-  new_title.classList.add("book-title");
-  const new_author = document.createElement("div");
-  new_author.classList.add("book-author");
-  const new_pages = document.createElement("div");
-  new_pages.classList.add("book-pages");
-  const new_isRead = document.createElement("div");
-  new_isRead.classList.add("book-isRead");
-  const button = document.createElement("button");
-  button.classList.add("delete");
-
-  new_title.textContent = `Title : ${book.title}`;
-  new_author.textContent = `Author :${book.author}`;
-  new_pages.textContent = `Number of pages :${book.pages}`;
-  new_isRead.textContent = `${book.isRead}`;
-    button.textContent='Delete'
+}
+function removeBook(i){
+    myLibrary.splice(i, 1);
+    render();
+}
 
 
-  book_container.appendChild(new_Book);
-  new_Book.appendChild(new_title);
-  new_Book.appendChild(new_author);
-  new_Book.appendChild(new_pages);
-  new_Book.appendChild(new_isRead);
-  new_Book.appendChild(button);
+function addBookToLibrary() {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const isRead = document.querySelector("#isRead").checked
+      ? "has been read"
+      : "not read yet";
+    const abook = new Book(title, author, pages, isRead);
+    myLibrary.push(abook);
+    render();
+  }
 
-
-document.querySelector(".delete").addEventListener("click", function(){
-    book_container.removeChild(new_Book);
-})
-dialog.close();
-reset();
- 
+btn.addEventListener("click", () => {
+    addBookToLibrary();
+    dialog.close();
+    reset();
 });
+Book.prototype.toggleRead = function(){
+    this.read = !this.read;
+}
 
 function reset(){
      document.querySelector("#title").value = '';
@@ -72,5 +83,3 @@ function reset(){
         document.querySelector("#pages").value = '';
          document.querySelector("#isRead").checked=false;
 }
-
-
